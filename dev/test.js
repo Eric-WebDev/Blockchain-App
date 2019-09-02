0bd9e1b2452856974a05ccc2cc9da6708a321ffd
arek512 <35996962+arek512@users.noreply.github.com>
2019-09-02 21:17:52 +0100

    blockchain basic methods

---
+++ dev/test.js
@@ -0,0 +1,23 @@
+const Blockchain = require("./blockchain");
+const block = new Blockchain();
+
+// const previousBlockHash = "  AKJDH4U4HDHDD4D4";
+// const currentBlockData = [
+//   {
+//     amount: 10,
+//     sender: "jdjjddjehr3334334",
+//     recipient: "jjjhghg4343bhjjej"
+//   },
+//   {
+//     amount: 23,
+//     sender: "jdffddjehr3334334",
+//     recipient: "jjjhghg4343bhjjej"
+//   },
+//   {
+//     amount: 18,
+//     sender: "j45tfderfhr3334334",
+//     recipient: "jjjhgfffrhg4343bhjjej"
+//   }
+// ];
+
+console.log(block);
